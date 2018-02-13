3fec02281b4ff83258a24452d41ca25309e6a206
greg <gregpmiller@gmail.com>
2018-02-13 15:07:42 -0400

    added a user route

--- app.js
+++ app.js
@@ -2,15 +2,17 @@ const express = require('express')
 const logger = require  ('morgan')
 const bodyParser = require('body-parser')
 const app = express()
+const user_routes = require('./routes/user')
 
 app.use(logger('dev'))
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-
 app.get('*', (req,res) => res.status(200).send({
   message: 'Catch-all route reached Software Engineering!'
 }))
+
+app.get('/users', user_routes)
 
 module.exports = app;
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const user_controller = require('../controllers/user');
 
 router.use((req,res,next) => {
   console.log('Time: ', Date.now())
