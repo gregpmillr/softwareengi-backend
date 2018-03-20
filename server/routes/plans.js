9163fda9bead92944560b2d4808129eb20f0a6ab
greg <gregpmillr@gmail.com>
2018-03-20 23:47:13 +0000

    updated api for getting a plan

--- server/routes/plans.js
+++ server/routes/plans.js
@@ -7,7 +7,7 @@ router.use((req,res,next) => {
   next()
 })
 
-router.get('/', plans_controller.getAll);
+router.get('/:username/list', plans_controller.getAll);
 
 router.get('/:id', plans_controller.get);
 
