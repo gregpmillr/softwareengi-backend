7cf4a3e51c21834e114a45ce21dc827b7b866f50
greg <gregpmiller@gmail.com>
2018-03-20 22:32:51 -0300

    updated models

--- server/migrations/20180219014327-create-plan.js
+++ server/migrations/20180219014327-create-plan.js
@@ -2,6 +2,12 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.createTable('plans', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
       title: {
         allowNull: false,
         type: Sequelize.STRING,
