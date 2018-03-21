d8d7388978c5705f323e4c52d032eb9ebccd8115
greg <gregpmiller@gmail.com>
2018-03-21 01:47:14 -0300

    got rid of error messaging

--- server/controllers/plans.js
+++ server/controllers/plans.js
@@ -14,7 +14,6 @@ exports.getAll = (req, res, next) => {
   .then((user) => {
     user.getPlans()
       .then((plans) => {
-        console.log(plans);
         res.status(200).json(plans)
       })
       .catch((err) => {
@@ -37,37 +36,28 @@ exports.delete = (req,res,next) => {
     attributes: ['id','title']
   })
   .then((plan) => {
-    console.log('1')
     return UserPlan.findOne({
       where: {
         plan_id : plan.id
       }
     })
     .then((userPlan) => {
-      console.log('2')
       return userPlan.destroy()
     })
     .catch((err) => {
-      console.log('3')
       throw err
     })
     .then(() => {
-      console.log('4')
       return plan.destroy()
     })
     .catch((err) => {
-      console.log('5')
       throw err
     })
   })
   .catch((err) => {
-    console.log('6')
-    console.log(err)
     res.status(400).json(err)
   })
   .then(() => {
-    console.log('7')
-    console.log('this is a test')
     res.status(200).json({status: "ok"})
   })
 
